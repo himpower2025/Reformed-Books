c5b15f48e2683118282ec6b73c99cdbe84a52b84
Jae Woo <himpower2025@gmail.com>
2026-06-28 23:35:33 +0545

    logo 3

--- components/header.tsx
+++ components/header.tsx
@@ -30,8 +30,8 @@ function AdaptiveLogo() {
       <Image
         src="/logo-icon.png"
         alt="Reformed Books Logo"
-        width={70}
-        height={70}
+        width={40}
+        height={40}
         className="object-contain"
         priority
       />
